14cb3dde50123acccd367dcb4591ee9d667c6b76
samontoyapa <samontoyapa@unal.edu.co>
2020-02-01 22:13:39 -0500

    generarJWT

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const jwt = require('jsonwebtoken');
+const jwt2 = require('jwt-simple');
+const moment = require('moment');
+const config = require('../config');
 
 
 function verifyToken(req, res, next){
@@ -15,4 +18,45 @@ function verifyToken(req, res, next){
         next();
 
     }else {    res.sendStatus(403); }
+}
+
+function createToken(user){
+    const  payload = {
+
+        sub: user._identificacion,
+        iat: moment().unix(),
+        exp: moment().add(14,'days').unix(),
+    }
+
+    return jwt2.encode(payload, config.SECRET_TOKEN)
+
+}
+
+function decodeToken (token) {
+    const decoded = new Promise((resolve, reject) => {
+        try{
+            const payload = jwt2.decode(token, config.SECRET_TOKEN)
+
+            if (payload.exp <= moment().unix()) {
+                reject({
+                    status: 401,
+                    message: 'El token ha expirado'
+                })
+            }
+            resolve(payload.sub)
+        }catch (err) {
+            reject ({
+                status: 500,
+                message:'Token invalido'
+
+            })
+        }
+    })
+
+    return decoded
+}
+
+module.exports = {
+    createToken,
+    decodeToken
 }
