6b1726d4cb29bbb61901b3a44fd2d8d3aa7d6f2f
Santiago Rendon <santirg2000@gmail.com>
2020-03-01 20:00:52 -0500

    Auto stash before merge of "master" and "origin/master"
    Fix de count de usuarios.

--- controllers/user.js
+++ controllers/user.js
@@ -157,15 +157,21 @@ exports.getNonSubUsers = async (req, res) => {
         userQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
     }
     userQuery
-    .then(documents => {
-        fetchedUsers = documents;
-        return User.count();
-    })
-    .then(count => {
-        res.status(200).json({
-            message: "Users fetched successfully!",
-            users: fetchedUsers,
-            maxUsers: count
+        .then(documents => {
+            fetchedUsers = documents;
+            return fetchedUsers.length;
+        })
+        .then(count => {
+            res.status(200).json({
+                message: "Users fetched successfully!",
+                users: fetchedUsers,
+                maxUsers: count
+            });
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: "Fetching users failed!"
+            });
         });
     })
     .catch(error => {
