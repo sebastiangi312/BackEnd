8d32bbe4493ac7107960ff5e747b374e84629fa0
cara994923 <cara994923@gmail.com>
2020-03-14 15:13:08 -0500

    modificacion lottery

--- controllers/lottery.js
+++ controllers/lottery.js
@@ -63,6 +63,13 @@ exports.closeLottery = async (req, res) => {
                 const user = await User.findById(firstPrizeWinners[ip1]);
                 var newBalance = user.balance + (firstPrize/firstPrizeWinners.length); 
                 const results = await User.updateOne({ _id: firstPrizeWinners[ip1]}, { balance: newBalance});
+                if (results.n > 0) {
+                    res.status(200).json({ message: 'Se repartieron los respectivos premios' });
+                } else {
+                    res.status(500).json({
+                        message: "Error al repartir los premios",
+                    });
+                }
                 ip1 = ip1 + 1;
              });
                var ip2 = 0;
@@ -70,6 +77,13 @@ exports.closeLottery = async (req, res) => {
                 const user = await User.findById(secondPrizeWinners[ip2]);
                 var newBalance = user.balance + (secondPrize/secondPrizeWinners.length); 
                 const results = await User.updateOne({ _id: secondPrizeWinners[ip2]}, { balance: newBalance});
+                if (results.n > 0) {
+                    res.status(200).json({ message: 'Se repartieron los respectivos premios' });
+                } else {
+                    res.status(500).json({
+                        message: "Error al repartir los premios",
+                    });
+                }
                 ip2 = ip2 + 1;
              });
              var ip3 = 0;
@@ -77,8 +91,15 @@ exports.closeLottery = async (req, res) => {
                 const user = await User.findById(thirdPrizeWinners[ip3]);
                 var newBalance = user.balance + thirdPrize; 
                 const results = await User.updateOne({ _id: secondPrizeWinners[ip3]}, { balance: newBalance});
+                if (results.n > 0) {
+                    res.status(200).json({ message: 'Se repartieron los respectivos premios' });
+                } else {
+                    res.status(500).json({
+                        message: "Error al repartir los premios",
+                    });
+                }
                 ip3 = ip3 + 1;
-                totalThridPrize = totalThridPrize + thirdPrize
+                totalThridPrize = totalThridPrize + thirdPrize;
              });
             });
 
