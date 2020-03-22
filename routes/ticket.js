e9086dfeeed431382e335ebbea56a6179640dfa9
David González <dagonzalezce@unal.edu.co>
2020-03-22 14:38:38 -0500

    Se crea middleware que verifica que usuario es bettor

--- routes/ticket.js
+++ routes/ticket.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const TicketController = require("../controllers/ticket");
 const checkAuth = require('../middleware/check-auth');
+const checkAuthBettor = require('../middleware/check-auth-bettor');
 
 const router = express.Router();
 
-router.post("/lotteryTicket", checkAuth, TicketController.createLotteryTicket);
+router.post("/lotteryTicket", checkAuth, checkAuthBettor ,TicketController.createLotteryTicket);
 
 module.exports = router;
